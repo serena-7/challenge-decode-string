ddbee2ad4dfd0f19a0098abe410a40a4ebf89fdb
Serena Sorensen <serena.sorensen7@gmail.com>
2022-04-11 10:05:58 -0600

    challenge complete

--- decodeString.js
+++ decodeString.js
@@ -1,1 +1,13 @@
-// Write your code below this line
+function decoder(str){
+    let arr = str.split('');
+    let num = Number(arr[0]);
+    let newArr = [];
+    for(let i = 1; i < arr.length; i++){
+        charCode = arr[i].charCodeAt(0);
+        newCode = charCode + num;
+        newArr.push(String.fromCharCode(newCode));
+    }
+    return newArr.join('');
+}
+
+console.log(decoder("2fcjjm"));
